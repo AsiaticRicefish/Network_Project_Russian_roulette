5e236f14119bd14cc268a4e69b4e8f2d95f6af4f
rnrgll <shuajjjjj@naver.com>
2025-07-23 15:55:53 +0900

    [Feat] SoundManager 기본 구조 및 볼륨 설정, 재생 기능 구현
    - BGM, SFX 볼륨 설정 : SetBgmVolume(float), SetSfxVolume(float)
    - AudioClip 전달 방식으로 사운드 재생 : Play(AudioClip, SoundType)
    - 모든 사운드 초기화 : Clear()
    - Resources 폴더 내 사운드 파일 경로를 캐싱하는 기능은 제외

---
+++ Assets/LDH/LDH_Scripts/Utils/Define_LDH.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public class Define_LDH : MonoBehaviour
+    {
+        public enum Sound
+        {
+            Bgm,
+            Sfx,
+            MaxCount,
+        }
+
+    }
+}
