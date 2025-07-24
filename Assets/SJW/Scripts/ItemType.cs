03bb490cadb796b5c8489ac4c52b740529cdbccf
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-07-24 11:12:54 +0900

    [Feat] 아이템매니저 / 아이템타입 생성

---
+++ Assets/SJW/Scripts/ItemType.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Cigarette, // 담배
+    Cuffs, // 수갑
+    Dial, // 다이얼
+    Cellphone, // 휴대폰
+}
