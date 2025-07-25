0ece79a3c8b49058493377a9262afd3c11d6de96
rnrgll <shuajjjjj@naver.com>
2025-07-25 17:22:05 +0900

    [Feat] AnimationBuilder 클래스 도입 및 연출 체이닝 시스템 구현
    - FadeIn, FadeOut, MoveTo, MoveBy, Shake, ScalePunch 등 DOTween 연출 기능을 AnimationBuilder로 통합
    - UI(RectTransform) / 월드 오브젝트(Transform) 대상 자동 처리 분기
    - Delay, OnComplete, Kill, SetEase 등 유틸리티 메서드 추가

---
+++ Assets/LDH/LDH_Scripts/Managers/AnimationManager.cs
@@ -0,0 +1,15 @@
+using DesignPattern;
+using UnityEngine;
+using DG.Tweening;
+using LDH_Animation;
+
+namespace Managers
+{
+    public class AnimationManager : Singleton<AnimationManager>
+    {
+        public AnimationBuilder Create(Transform target)
+        {
+            return new AnimationBuilder(target);
+        }
+    }
+}
