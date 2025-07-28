e1fade58ff6a8a72ea4e40826867f7b72e7eab94
neosmu <hojunglee00@naver.com>
2025-07-28 15:16:43 +0900

    [Fix] FireSync 수정
    테스트 진행하였을때 탄창에 들어있는 탄 개수가 다르게 적용이 되어서 마스터 클라이언트만 탄창 장전하고 다른 클라이언트는 탄창의 정보를 받아서 진행하는 형식으로 변경

--- Assets/LHJ/LHJ_Scripts/FireSync.cs
+++ Assets/LHJ/LHJ_Scripts/FireSync.cs
@@ -24,7 +24,19 @@ public class FireSync : MonoBehaviourPun
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
-            photonView.RPC("Reload", RpcTarget.All);
+            // 마스터 클라이언트만 탄창 장전
+            if (PhotonNetwork.IsMasterClient)
+            {
+                GunManager.Instance.Reload();
+
+                // 클라이언트에게 탄창 정보 전달
+                List<int> bulletList = new();
+                foreach (var bullet in GunManager.Instance.Magazine)
+                {
+                    bulletList.Add((int)bullet);
+                }
+                photonView.RPC("Reload", RpcTarget.All, bulletList.ToArray());
+            }
         }
     }
 
@@ -42,15 +54,25 @@ public class FireSync : MonoBehaviourPun
             Debug.LogError($"{targetId}이 데미지를 입었습니다.");
         else
             Debug.LogError($"{targetId}이 데미지를 입지않았습니다.");
-        // 발사 처리
+        // 발사 처리 타겟현재 null
         GunManager.Instance.Fire(null);
+
+        //_loadedBullet 보호수준떄문에 주석처리
         //Debug.LogError($"장전된 탄: {GunManager.Instance._loadedBullet}, 남은 탄 수: {GunManager.Instance.Magazine.Count}");
         // InGameManager.Instance.EndTurn();     // 게임 매니저 EndTurn호출
     }
     [PunRPC]
-    private void Reload()
+    private void Reload(int[] bullets)
     {
-        GunManager.Instance.Reload();
-        //Debug.LogWarning($"장전 완료! 남은 탄 수: {GunManager.Instance.Magazine.Count}, 첫 탄: {GunManager.Instance._loadedBullet}");
+        // 기존 탄창 초기화 
+        GunManager.Instance.Magazine.Clear();
+        
+        // 배열로 탄창 구성
+        foreach (int b in bullets)
+        {
+            GunManager.Instance.Magazine.Enqueue((BulletType)b);
+        }
+        //_loadedBullet 보호수준떄문에 주석처리
+        //Debug.LogWarning($"남은 탄 수: {GunManager.Instance.Magazine.Count}, 첫 탄: {GunManager.Instance._loadedBullet}");
     }
 }
