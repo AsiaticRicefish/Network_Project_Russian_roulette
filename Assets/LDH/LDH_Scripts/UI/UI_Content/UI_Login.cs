4116497012ae7f85d9dc86c975ea283e8c63900d
rnrgll <shuajjjjj@naver.com>
2025-07-29 01:17:12 +0900

    [Feat] Title, Login, Signup UI 기능 구현
    - 타이틀 화면에서 영상 재생 후 로그인/회원가입 진입 흐름 구현
      - VideoPlayer 종료 시 타이틀 패널 활성화
      - 로그인/회원가입 버튼 클릭 시 해당 패널 열림 (애니메이션 포함)
    - 로그인 UI
      - 이메일, 비밀번호 입력 후 확인
      - 로그인 성공 시 씬 이동, 실패 시 모달 출력
      - 입력 필드 초기화 시 placeholder 강제 리셋 처리
    - 회원가입 UI
      - 이름, 이메일, 비밀번호, 비밀번호 확인 입력
      - 이메일 유효성 확인 시 FeedbackButton 사용하여 결과 피드백
      - 회원가입 성공/실패에 따른 모달 출력 및 창 닫기 처리
    - Firebase 연동 전 임시 테스트 구조로 TempFirebaseManager 사용

---
+++ Assets/LDH/LDH_Scripts/UI/UI_Content/UI_Login.cs
@@ -0,0 +1,109 @@
+using Managers;
+using Michsky.UI.ModernUIPack;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+using UIManager = Managers.UIManager;
+
+namespace GameUI
+{
+    public class UI_Login : MonoBehaviour
+    {
+        //------ temp variable for test ----- //
+        [Header("Test Login Result (Simulated)")]
+        [SerializeField] private bool _isSuccess;
+
+        //------ firebase 연동시 해당 변수는 삭제, 각 로그인 실패 상황에 따라 적절한 title, message로 설정해야 함 ----- //
+        [Header("Fail Modal (Test)")]
+        [SerializeField] private Define_LDH.NotifyType failType;
+        [SerializeField] private string _failTitle;
+        [SerializeField] private string _failMessage;
+        
+        [Header("Modal Position Offset")]
+        [SerializeField] private Vector2 _offset;
+
+        
+        [Header("UI Contents")] 
+        [SerializeField] private TMP_InputField _emailField;
+        [SerializeField] private TMP_InputField _passwordField;
+        
+        
+        private ModalWindowManager _loginModal;
+
+
+        private void Awake() => Init();
+        private void Start() => Subscribe();
+        
+        private void Init()
+        {
+            _loginModal = GetComponent<ModalWindowManager>();
+        }
+
+        private void Subscribe()
+        {
+            _loginModal.onConfirm.AddListener(OnLoginConfirm);
+            _loginModal.onCancel.AddListener(Clear);
+        }
+
+        
+        /// <summary>
+        /// 로그인 결과
+        /// </summary>
+        public void OnLoginConfirm()
+        {
+            //todo: 로그인 로직과 연결 필요
+
+            if (_isSuccess)
+            {
+                Debug.Log($"[{GetType().Name}] 로그인에 성공하여 씬 이동합니다.");
+                SceneManager.LoadSceneAsync("Lobby");
+            }
+            else
+            {
+                Debug.Log($"[{GetType().Name}] 로그인 실패");
+                ShowModal(failType, _failTitle, _failMessage);
+            }
+        }
+
+
+        /// <summary>
+        /// 모달 생성 및 표시
+        /// </summary>
+        private void ShowModal(Define_LDH.NotifyType type, string title, string description)
+        {
+            UI_Modal modal = Manager.UI.SpawnPopupUI<UI_Modal>("UI_SlidingModal");
+            
+            //content 데이터 설정
+            modal.SetContent(type, title, description);
+
+            RectTransform modalRect = modal.GetComponent<RectTransform>();
+            
+            //rect transform 설정
+            UIManager.SetRightBottom(modalRect, modalRect.rect.size, new Vector2(_offset.x, _offset.y));
+            
+            //모달 보이게
+            modal.Show();
+        }
+        
+        
+        /// <summary>
+        /// 입력값 초기화
+        /// </summary>
+        public void Clear()
+        {
+            _emailField.text
+                = _passwordField.text
+                    = "";
+            
+            _emailField.gameObject.SetActive(false);
+            _passwordField.gameObject.SetActive(false);
+            
+            _emailField.gameObject.SetActive(true);
+            _passwordField.gameObject.SetActive(true);
+            
+        }
+        
+    }
+}
