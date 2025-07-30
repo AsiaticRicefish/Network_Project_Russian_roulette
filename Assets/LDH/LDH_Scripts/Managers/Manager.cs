451ed33c89420fac2dc24f5c68defd6327554b13
rnrgll <shuajjjjj@naver.com>
2025-07-30 12:45:32 +0900

    [Create] Lobby UI 작업 및 프리팹 생성

--- Assets/LDH/LDH_Scripts/Managers/Manager.cs
+++ Assets/LDH/LDH_Scripts/Managers/Manager.cs
@@ -46,11 +46,11 @@ namespace Managers
 
         public static GunManager Gun => GunManager.Instance;            // 총 매니저
 
-        public static PlayerManager PlayerManager => PlayerManager.Instance;   // 플레이어 매니저
+        //public static PlayerManager PlayerManager => PlayerManager.Instance;   // 플레이어 매니저
 
         public static InGameManager Game => InGameManager.Instance;         // 게임 매니저
 
-        public static ItemBoxSpawnerManager ItemBoxSpawner => ItemBoxSpawnerManager.Instance; // 아이템 박스 스폰 매니저
+        //public static ItemBoxSpawnerManager ItemBoxSpawner => ItemBoxSpawnerManager.Instance; // 아이템 박스 스폰 매니저
         public static ItemDatabaseManager ItemDatabase => ItemDatabaseManager.Instance; // 아이템 데이터베이스 매니저
         public static ItemManager Item => ItemManager.Instance; // 아이템 매니저
         public static DeskUIManager DeskUI => DeskUIManager.Instance; // 아이템 슬롯 UI 매니저
@@ -67,7 +67,7 @@ namespace Managers
             //각각의 매니저 스크립트를 프리팹에 스크립트를 직접 추가해두거나 아래와 같이 AddComponent로 동적으로 추가한다.
             manager.AddComponent<TestManager>();
             manager.AddComponent<SoundManager>();
-            manager.AddComponent<PlayerManager>();
+            //manager.AddComponent<PlayerManager>();
             manager.AddComponent<UIManager>();
             manager.AddComponent<ItemSyncManager>();
         }
